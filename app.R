758be928c6d0a709987ed1ad6750a66c69fa82f8
yllz <31703150+yllz@users.noreply.github.com>
2018-01-20 21:54:30 -0800

    Add app code

---
+++ app.R
@@ -0,0 +1,73 @@
+# load all the packages needed
+library(shiny)
+library(shinyjs)
+library(ggplot2)
+library(dplyr)
+library(rsconnect)
+
+
+ui <- fluidPage(
+  
+  # App title
+  titlePanel("Wine Shiny"),
+  
+  # Sidebar layout with input definitions
+  sidebarLayout(
+    sidebarPanel(
+      img(src = "drink.jpg", width = "100%"),
+      selectInput("countryInput", "Country",
+                  choices = c("Argentina", "Australia", "Austria", "Chile",
+                              "France", "Italy", "New Zealand", "Portugal",
+                              "Spain", "US"),
+                  selected = "France"),
+      br(),
+      colourInput("col", "Selected colour for histogram", "violetred4")
+    ),
+    
+    # Main panel layout with output definitions
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Boxplot_Price", plotOutput("Boxplot_Price")),
+        tabPanel("Points", plotOutput("Points")),
+        tabPanel("Variety", plotOutput("Variety")))
+    )
+  )
+)
+
+dat <- read.csv("wine_filtered.csv", header=TRUE, sep=",")
+dat$price_group = cut(dat$price,c(0,50,500,1000,1500,2000,2300))
+levels(dat$price_group) = c("Very Cheap","Cheap","Inexpensive","Average","Expensive","Very Expensive")
+
+# Define server for the app
+server <- function(input, output) {
+  
+  filtered <- reactive({
+    
+    if(is.null(input$countryInput)) {
+      return(NULL)
+    }
+    
+    dat %>%
+      filter(country == input$countryInput)
+  })
+  
+  # create the output plots
+  output$Boxplot_Price <- renderPlot({
+    ggplot(filtered(), aes(x = country, y = price)) +
+      geom_boxplot()
+  })
+  
+  output$Points <- renderPlot({
+    ggplot(filtered(), aes(x = points, y = price, col=price_group)) +
+      geom_point()
+  })
+  
+  output$Variety <- renderPlot({
+    ggplot(filtered(), aes(log(price))) +
+      geom_histogram(binwidth=0.2, fill = input$col) +
+      facet_wrap(~variety)
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
